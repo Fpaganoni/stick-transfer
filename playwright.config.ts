76922d2b3743db0497b149054d80627afe4ac217
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2026-05-20 15:19:05 +0200

    Feat: Solved issues for CI/CD properly

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,12 +18,10 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
   ],
-  webServer: process.env.CI
-    ? undefined
-    : {
-        command: "pnpm dev",
-        url: "http://localhost:3000",
-        reuseExistingServer: !process.env.CI,
-        timeout: 120_000,
-      },
+  webServer: {
+    command: process.env.CI ? "pnpm start" : "pnpm dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000,
+  },
 });
